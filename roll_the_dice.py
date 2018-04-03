1592621ecffddfedf26c1a85fc74ab71623f5026
Elicia Au Duong <eliciaauduong@outlook.com>
2018-04-04 09:34:19 +1000

    GPN Term 4 2015 - Roll the Dice

---
+++ roll_the_dice.py
@@ -0,0 +1,11 @@
+#made by Elicia Au Duong 04.April.2018
+
+import random
+
+throw = int(input("How many dice would you like to throw? "))
+faces = int(input("How many sides on each die? "))
+
+
+# for i in range(1, throw + 1):
+#     roll = random.choice(range(1, faces + 1))
+#     print("Dice #" + str(i) + " rolls: " + str(roll))
